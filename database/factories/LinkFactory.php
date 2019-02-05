aa4301815944e0b0305a33669f0a423614991afd
biniyam <abrandnewb@gmail.com>
2019-02-05 12:05:22 +0100

    initial

---
+++ database/factories/LinkFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+$factory->define(App\Link::class, function (Faker\Generator $faker) {
+    return [
+        'title' => substr($faker->sentence(2), 0, -1),
+        'url' => $faker->url,
+        'description' => $faker->paragraph,
+    ];
+});
